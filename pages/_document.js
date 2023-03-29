93b810c759215d179508a121789624ae6ce19710
Deepak <deepakgangwar4265@gmail.com>
2023-03-29 18:31:24 +0530

    Fix jsconfig, clean up some code, ready as starting point

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+// Pages in Next.js skip the definition of the surrounding document's markup. 
+// For example, you never include < html >, <body>, etc. To override that default 
+// behavior, you must create a file at./ pages / _document.js, where you can extend 
+// the Document class.
+
+// https://nextjs.org/docs/advanced-features/custom-document
+// https://stackoverflow.com/questions/51040669/next-js-use-of-app-js-and-document-js
+
 export default function Document() {
   return (
     <Html lang="en">
