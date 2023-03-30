2f6ccddae55cd361b19a9d24ee7afc8343b13d84
Deepak <deepakgangwar4265@gmail.com>
2023-03-30 21:06:08 +0530

    Add components dir and basic navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
+import Nav from '@/component/nav'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Nav />
+      <Component {...pageProps} />
+    </div>
+  )
 }
