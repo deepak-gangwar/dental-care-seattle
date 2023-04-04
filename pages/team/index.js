9a9e06b8997beed15b7746d59f98fabd96cb724c
Deepak <deepakgangwar4265@gmail.com>
2023-04-04 15:30:20 +0530

    refactor: clean some markup in teams page

--- pages/team/index.js
+++ pages/team/index.js
@@ -1,32 +1,42 @@
+import Head from "next/head"
 import Link from "next/link"
 import { sanityClient, urlFor } from "lib/sanity"
 import Image from "next/image"
 
-export default function Team({ results }) {
+export default function Team({ doctors }) {
   return (
     <>
-      <h1>Team</h1>
-      <ul>
-        {results?.length > 0 && results.map(result => (
-          <li key={result._id}>
-            {/* Note: To render bio correctly, you would need to use portable text, for that you have to watch kapehe's rest of the video */}
-            <Image src={urlFor(result.image).url()} alt={result.name} width={300} height={440} />
-            <br />
-            <span>Name - </span><span>{result.name}</span>
-            <br />
-            <span>Title - </span><span>{result.title}</span>
-            <br />
-            <span>Description - </span><span>{result.description}</span>
-            <br />
-            <span>Specialty - </span><span>{result.specialty}</span>
-            <hr />
-            <br />
-            <br />
-            <br />
-            <br />
-          </li>
-        ))}
-      </ul>
+      <Head>
+        <title>Meet Your Dentists — Dental Care Seattle</title>
+        <meta name="description" content="Our team of friendly, gentle dentists at Dental Care Seattle." />
+      </Head>
+      <main>
+        <h1>Team</h1>
+        <ul>
+          {doctors?.length > 0 && doctors.map(result => (
+            <li key={result._id}>
+              {/* Note: To render bio correctly, you would need to use portable text, for that you have to watch kapehe's rest of the video */}
+              <Image src={urlFor(result.image).url()} alt={result.name} width={300} height={440} />
+              <br />
+              <span>Name - </span><span>{result.name}</span>
+              <br />
+              <span>Title - </span><span>{result.title}</span>
+              <br />
+              <span>Description - </span><span>{result.description}</span>
+              <br />
+              <span>Specialty - </span><span>{result.specialty}</span>
+              <br />
+              <br />
+              <Link href="/"><button>Learn more</button></Link>
+              <hr />
+              <br />
+              <br />
+              <br />
+              <br />
+            </li>
+          ))}
+        </ul>
+      </main>
     </>
   )
 }
@@ -42,6 +52,6 @@ const homeQuery = `*[_type == "doctor"] {
 }`
 
 export async function getStaticProps() {
-  const results = await sanityClient.fetch(homeQuery)
-  return { props: { results } }
+  const doctors = await sanityClient.fetch(homeQuery)
+  return { props: { doctors } }
 }
