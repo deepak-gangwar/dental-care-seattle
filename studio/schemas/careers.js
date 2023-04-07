4574481d73ca0eb1e50cb8547ab81d56cb716677
Deepak <deepakgangwar4265@gmail.com>
2023-04-07 14:56:17 +0530

    Add careers schema and relevant data

---
+++ studio/schemas/careers.js
@@ -0,0 +1,32 @@
+export default {
+    name: 'careers',
+    title: 'Careers',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+        },
+        {
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: {
+                source: 'title',
+                maxLength: 96,
+            },
+        },
+        {
+            name: 'team',
+            title: 'Team',
+            type: 'string',
+        },
+        {
+            name: 'description',
+            title: 'Description',
+            type: 'array',
+            of: [{ type: 'block' }],
+        },
+    ],
+};
--- studio/schemas/index.js
+++ studio/schemas/index.js
@@ -1,5 +1,6 @@
 import page from "./page"
 import services from "./services"
 import doctor from "./doctor"
+import careers from "./careers"
 
-export const schemaTypes = [page, services, doctor]
+export const schemaTypes = [page, services, doctor, careers]
