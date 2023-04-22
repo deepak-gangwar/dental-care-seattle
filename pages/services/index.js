448591ecd306c670660f7c49aa15a9e5ac1067ed
Deepak <deepakgangwar4265@gmail.com>
2023-04-22 18:36:24 +0530

    Extract service into its own component

--- pages/services/index.js
+++ pages/services/index.js
@@ -3,9 +3,10 @@ import Head from "next/head"
 import Image from "next/image"
 import Link from "next/link"
 import { useEffect, useState } from "react"
+import Service from "@/component/pages/services/Service"
 
 export default function Services() {
-  const [current, setCurrent] = useState(1)
+  const [current, setCurrent] = useState(0)
 
   function goToPrev() {
     onCurrentItemChange(-1)
@@ -66,74 +67,18 @@ export default function Services() {
               <li
                 key={index}
                 className={current === index ? 'current' : ''}
-              // onClick={() => handleItemClick(index)}
               >
                 {item}
               </li>
             ))}
-            {/* <li className="carousel_item"></li>
-            <li className="carousel_item"></li>
-            <li className="carousel_item"></li>
-            <li className="carousel_item"></li> */}
           </ul>
 
 
 
           <div className={styles.items}>
             {/* Items are the number of instagram stories you want to add */}
-            <div className={styles.item}>
-
-              {/* ============= PUBLISHER PROFILE ============= */}
-
-              <div className={styles.publisher}>
-                <picture type="image" className={styles.avatar}>
-                  {/* <Image src="" alt="avatar for cosmetic dentistry" /> */}
-                </picture>
-                <div className={styles.author}>
-                  <span className={styles.author_line}>Cosmetic Dentistry</span>
-                </div>
-              </div>
-
-
-              {/* ============= POST ============= */}
-              <div className={styles.post}>
-                <a href="">
-                  <h2 className={styles.title}>
-                    {/* These are split text divs */}
-                    <div className={styles.title_mask}>
-                      <div className={styles.title_line}>At Dental Care Seattle, cosmetic</div>
-                    </div>
-                    <div className={styles.title_mask}>
-                      <div className={styles.title_line}>dentistry by our doctors can offer</div>
-                    </div>
-                    <div className={styles.title_mask}>
-                      <div className={styles.title_line}>you a head turning smile.</div>
-                    </div>
-                  </h2>
-                </a>
-
-                {/* Add the button component here */}
-                <Link className={styles.link} href="">
-                  <div className={styles.wrap}>
-                    <div className={styles.circle}>
-                      <div className={styles.circle_fill}></div>
-                      <svg viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg" className={styles.circle_outline}><circle cx="25" cy="25" r="23"></circle></svg>
-                      <div className={styles.circle_icon}>
-                        <svg viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg" className={styles.icon_arrow}><path d="M7.8816 0L7.8958 6.67451H6.5893V2.24377L0.937271 7.91L0 6.97273L5.66623 1.3065H1.23549V0H7.8816Z" fill="currentColor"></path></svg>
-                      </div>
-                    </div>
-                    <div className={styles.label}>
-                      See all treatments
-                      <div className={styles.border}></div>
-                    </div>
-                  </div>
-                </Link>
-              </div>
-
-
-              {/* ============= MEDIA ============= */}
-              <div className={styles.media}></div>
-            </div>
+            <Service />
+            <Service />
           </div>
 
           {/* ===============  Story Navigation  =============== */}
