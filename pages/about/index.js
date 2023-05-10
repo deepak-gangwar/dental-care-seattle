74d88e192d3ba82bcf1755beec71f0eb785a0d0f
Deepak <deepakgangwar4265@gmail.com>
2023-05-10 11:36:55 +0530

    Some minor changes in layout and some cleanup

--- pages/about/index.js
+++ pages/about/index.js
@@ -4,6 +4,7 @@ import Section1 from "@/component/pages/about/Section1"
 import Dots from "@/component/Dots"
 import Section2 from "@/component/pages/about/Section2"
 import Section3 from "@/component/pages/about/Section3"
+// import Section4 from "@/component/pages/about/section4"
 
 export default function About() {
   return (
@@ -14,11 +15,12 @@ export default function About() {
         <meta name="description" content="Meet your top-rated Seattle practice for cosmetic, restorative, and preventative dentistry on First Hill. You can expect the best care, gentle treatment, and beautiful results from your dentist in Seattle!" />
       </Head>
       <div id='about' className='page'>
-        <Dots />
+        {/* <Dots /> */}
         <Hero />
         <Section1 />
         <Section2 />
         <Section3 />
+        {/* <Section4 /> */}
       </div>
     </>
 
