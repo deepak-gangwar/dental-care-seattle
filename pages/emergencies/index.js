7208314edc577c04b37fe594b84a6d967b26624f
Deepak <deepakgangwar4265@gmail.com>
2023-06-11 19:56:58 +0530

    Add emergencies page

--- pages/emergencies/index.js
+++ pages/emergencies/index.js
@@ -1,35 +1,36 @@
-import Footer from "@/component/footer"
 import Head from "next/head"
 import Link from "next/link"
+import Footer from "@/component/footer"
 
-export default function Payment() {
+export default function Emergencies() {
   return (
     <>
       <Head>
-        <title>Pay Online — Dental Care seattle</title>
+        <title>Emergency Dental Treatment — Dental Care seattle</title>
         <meta name="description" content="Meet your top-rated Seattle practice for cosmetic, restorative, and preventative dentistry on First Hill. You can expect the best care, gentle treatment, and beautiful results from your dentist in Seattle!" />
       </Head>
-      <div id='payment' className='page'>
-        {/* <Dots /> */}
+      <div id='emergencies' className='page'>
+        <div className="main_wrapper">
+          <h1>Dental Emergency? We can help.</h1>
+          <span>(206) 325-0166</span>
 
-        <div className="main__wrapper">
-          <h1>Make a Payment</h1>
-          <p>Use the button below to pay your bill online. Reference the “Chart No.” in the top-right corner of your most recent statement, and the current balance due.</p>
-          <h2>SECURE PAYMENTS</h2>
-          <p>Payments and patient information are processed securely through Authorize.net. We accept Visa, Mastercard, American Express, and Discover cards. </p>
-
-          {/* The payment button */}
-          <form data-preserve-html-node="true" name="PrePage" method="post" action="https://Simplecheckout.authorize.net/payment/CatalogPayment.aspx" id="yui_3_17_2_1_1686491703206_705">
-            <input data-preserve-html-node="true" type="hidden" name="LinkId" value="aafd7d94-bd8b-4863-976e-700af11d0807" /> <input data-preserve-html-node="true" type="submit" value="Pay Online " id="yui_3_17_2_1_1686491703206_704" class="" />
-          </form>
+          <div className="callout">
+            <h2>FIRST THING&apos;S FIRST—STAY CALM.</h2>
+            <p>Focus on stopping any bleeding and protecting the the injured area. If you&apos;re experiencing a dental emergency or need urgent care, give us a call at (206) 325-0166 and we&apos;ll discuss treatment options with you. If your emergency is life-threatening, dial 911 now or go to a hospital emergency room.</p>
+          </div>
         </div>
 
         <div className="sidebar">
-          <h3>Payment options</h3>
-          <p>
-            We also accept payments by phone, mail, or in the office at the time of service.
-            Please <span><Link href="/contact">Contact Us</Link></span> to discuss payment options, and read our <span><a href="https://www.dentalcareseattle.com/s/DentalCareSeattle_FinancialPolicy.pdf">Financial Policy</a></span> for more information.
-          </p>
+          <h3>Dental urgent care for: </h3>
+          <ul>
+            <li>RELIEF OF PAIN</li>
+            <li>LOST FILLING OR CROWN</li>
+            <li>BROKEN TOOTH</li>
+            <li>LOOSE OR KNOCKED OUT TOOTH</li>
+            <li>TOOTHACHE</li>
+            <li>SWELLING OR ABSCESS</li>
+            <li>TRAUMA TO THE MOUTH</li>
+          </ul>
         </div>
         <Footer />
       </div>
