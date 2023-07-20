06d4199e2b79971587acb616f6c32b5653f2cbd1
Deepak <deepakgangwar4265@gmail.com>
2023-07-20 21:43:20 +0530

    Hero animation added to all pages

--- components/pages/careers/Hero.jsx
+++ components/pages/careers/Hero.jsx
@@ -10,13 +10,13 @@ export default function Hero() {
             {/* <span>Make</span> */}
             {/* <span className={styles.line}></span> */}
             {/* <br /> */}
-            <span>Careers with us</span>
-            <span className={styles.line}></span>
+            <span className="js-hero-split">Careers with us</span>
+            <span className={`js-hero-line ${styles.line}`}></span>
           </h1>
           <span className={styles.arrow}>
-            <Image src="icons/icon-arrow-diagonal.svg" width={21} height={20} alt=''></Image>
+            <Image className="arrow-icon" src="icons/icon-arrow-diagonal.svg" width={21} height={20} alt=''></Image>
           </span>
-          <p className={styles.paragraph}>
+          <p className={`js-split ${styles.paragraph}`}>
             We&apos;re hiring! Join our talented team of patient-first caregivers.
           </p>
         </div>
--- components/pages/faq/Hero.jsx
+++ components/pages/faq/Hero.jsx
@@ -7,16 +7,16 @@ export default function Hero() {
       <section className={styles.hero}>
         <div className={styles.content}>
           <h1 className={styles.heading}>
-            <span>FAQ</span>
-            <span className={styles.line}></span>
+            <span className="js-hero-split">FAQ</span>
+            <span className={`js-hero-line ${styles.line}`}></span>
             {/* <br /> */}
             {/* <span>325-0166</span> */}
             {/* <span className={styles.line}></span> */}
           </h1>
           <span className={styles.arrow}>
-            <Image src="icons/icon-arrow-diagonal.svg" width={21} height={20} alt=''></Image>
+            <Image className="arrow-icon" src="icons/icon-arrow-diagonal.svg" width={21} height={20} alt=''></Image>
           </span>
-          <p className={styles.paragraph}>
+          <p className={`js-split ${styles.paragraph}`}>
             Questions and Answers.
           </p>
         </div>
